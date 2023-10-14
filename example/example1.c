e8d922f5cc481c5e4c79f9b27b03268d83f9f272
markduckai <1453879902@qq.com>
2023-10-14 20:45:39 +0800

    dd

---
+++ example/example1.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        execlp("/bin/ls", "ls", "-l", "-a", NULL);
+    }
+    else
+    {
+        wait(NULL);
+        printf("Child Complete\n");
+    }
+
+    return 0;
+}
